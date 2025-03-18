9c0403865bc7789f5b7ceb49e2135dbfb383ab0a
nouran <nm2462674@gmail.com>
2025-03-19 01:16:39 +0200

    first commit

--- project/__init__.py
+++ project/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '700e4c00bca577bede8fff2e08d359c7f5dfa7f80bd6fe4de2d6d77f54981796'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:nouran@localhost/flask1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db" 
 
 db = SQLAlchemy(app)
 
