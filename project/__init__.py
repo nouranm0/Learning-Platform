d8e197e3f9c7bbdab688bd3d1a391bd68718cdb6
ramy409 <130874970+ramy409@users.noreply.github.com>
2025-05-11 21:18:59 +0300

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '700e4c00bca577bede8fff2e08d359c7f5dfa7f80bd6fe4de2d6d77f54981796'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db" 
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+migrate = Migrate(app, db)
 
 from project import routes
